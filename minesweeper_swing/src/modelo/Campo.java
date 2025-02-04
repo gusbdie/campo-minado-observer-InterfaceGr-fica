133a0c1b52a4fb0dcbe24d9e9d398d2f130dd153
gusbdie <gustavobdetrich@gmail.com>
2025-02-04 19:56:35 -0300

    Criei a interface CampoObservador para que possamos aplicá-la em todos os Observers e mais tarde definirmos o que será feito com cada. Fiz também um ENUM para definir cada evento e facilitar na determinação de quando cada evento ocorre. E na classe Campo, modifiquei o método abrir, alternarMarcado, setAberto, além de adicionar uma List de observadores junto com os métodos para registrar os observadores e outro para notificá-los(que será onde iremos colocar o ENUM

---
+++ minesweeper_swing/src/modelo/Campo.java
@@ -0,0 +1,139 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Campo {
+
+    private final int linha;
+    private final int coluna;
+
+    private boolean aberto;
+    private boolean minado;
+    private boolean marcado;
+
+    private List<Campo> vizinhos = new ArrayList<>();
+    private List<CampoObservador> observadores = new ArrayList<>();
+
+    Campo(int linha, int coluna){
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public void registrarObservador(CampoObservador observador){
+        observadores.add(observador);
+    }
+
+    private void notificarObservadores(CampoEvento evento){
+        observadores.stream().forEach(o -> o.eventoOcorreu(this, evento));
+    }// com esse this eu me refiro ao objeto atual que to trabalhando
+    //no caso a classe
+
+    boolean adicionarVizinho(Campo vizinho){
+        boolean linhaDiferente = this.linha != vizinho.linha;
+        boolean colunaDiferente = this.coluna != vizinho.coluna;
+        boolean diagonal = linhaDiferente && colunaDiferente;
+
+        int valorLinha = Math.abs(this.linha - vizinho.linha);
+        int valorColuna = Math.abs(this.coluna - vizinho.coluna);
+        int valorGeral = valorColuna + valorLinha;
+
+        if(valorGeral == 1 && !diagonal){
+            vizinhos.add(vizinho);
+            return true;
+        }else if(valorGeral == 2 && diagonal){
+            vizinhos.add(vizinho);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    void alternarMarcado(){
+        if(!aberto){
+            marcado = !marcado;
+
+            if(marcado){
+                notificarObservadores(CampoEvento.MARCAR);
+            } else{
+                notificarObservadores(CampoEvento.DESMARCAR);
+            }
+        }
+    }
+
+    boolean abrir(){
+        
+        if(!aberto && !marcado){
+            if(minado){
+                notificarObservadores(CampoEvento.EXPLODIR);
+                return true;
+            }
+
+            setAberto(true);
+
+            if(vizinhancaSegura()){
+                vizinhos.forEach(v -> v.abrir());
+            }
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    boolean vizinhancaSegura(){
+        return vizinhos.stream().noneMatch(v -> v.minado);
+    }
+
+    public boolean isMarcado(){
+        return marcado;
+    }
+
+    void setAberto(boolean aberto){
+        this.aberto = aberto;
+
+        if(aberto){
+            notificarObservadores(CampoEvento.ABRIR);
+        }
+    }
+    void minar(){
+        minado = true;
+    }
+
+    public boolean isMinado(){
+        return minado;
+    }
+
+    public boolean isAberto(){
+        return aberto;
+    }
+
+    public boolean isFechado(){
+        return !isAberto();
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    boolean objetivoAlcancado(){
+        boolean desvendado = !minado && aberto;
+        boolean protegido = minado && marcado;
+
+        return desvendado || protegido;
+    }
+
+    long minasNaVizinhanca(){
+        return vizinhos.stream().filter(v -> v.minado).count();
+    }
+
+    void reiniciar(){
+        aberto = false;
+        minado = false;
+        marcado = false;
+    }
+}
